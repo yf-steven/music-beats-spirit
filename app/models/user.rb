08f700bdef8e71c532c263a065ea33946a7264e2
yf-steven <music_beats_spirit@icloud.com>
2021-06-19 15:53:39 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  with_options presence: true do
+    validates :nickname, uniqueness: true
+    validates :introduce
+    validates :favorite
+    validates :sex_id
+    validates :age_id
+  end
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :sex
   belongs_to :age
-  
+
 end
