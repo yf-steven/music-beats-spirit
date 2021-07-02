044312ea67e6b33f8edf1b8e81da3303622624b0
yf-steven <music_beats_spirit@icloud.com>
2021-07-02 16:04:05 +0900

    コメント削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :report_find, only: [:create, :destroy]
 
   def create
-    @report = Report.find(params[:report_id])
     @comment = @report.comments.new(comment_params)
     if @comment.save
       redirect_to report_path(@report.id)
@@ -11,7 +11,17 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to report_path(@report.id)
+  end
+
   private
+
+  def report_find
+    @report = Report.find(params[:report_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id)
