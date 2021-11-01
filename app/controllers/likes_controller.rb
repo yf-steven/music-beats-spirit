502bb8a056d6a231b7e784400772e6cbb70fb5c4
yf-steven <music_beats_spirit@icloud.com>
2021-11-01 15:53:25 +0900

    非同期通信のいいね機能実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,15 @@ class LikesController < ApplicationController
   before_action :report_find,  only: [:create, :destroy]
 
   def create
-    Like.create(user_id: current_user.id, report_id: @report.id)
-    redirect_to report_path(@report.id)
+    @like = Like.new(user_id: current_user.id, report_id: @report.id)
+    @like.save
+    render :index
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, report_id: @report.id)
-    like.destroy
-    redirect_to report_path(@report.id)
-    
+    @like = Like.find_by(user_id: current_user.id, report_id: @report.id)
+    @like.destroy
+    render :index
   end
 
   private
